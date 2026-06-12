9b92b0a14303dd7a27ae727b4ac246e10ac9f644
abdirazak <abdurazak.awil1@gmail.com>
2026-06-12 23:56:00 +0300

    feat(transactions): add transactions and order-confirmation

---
+++ apps/www/src/collections/transactions/hooks/mark-order-paid.ts
@@ -0,0 +1,47 @@
+import { Order, Transaction } from "@/payload-types"
+import { CollectionAfterChangeHook } from "payload"
+
+// TODO: make it transactional, do not create a new transaction if the order is already paid or refunded or if the amount is not correct
+export const markOrderPaid: CollectionAfterChangeHook<Transaction> = async ({
+  operation,
+  req,
+  doc,
+  context,
+}) => {
+  if (operation !== "create") {
+    return
+  }
+
+  // verify the correct amount was paid
+  const isOrderPopulated = typeof doc.order === "object" && doc.order !== null
+  const orderId = typeof doc.order === "number" ? doc.order : doc.order.id
+
+  let order: Order
+
+  if (isOrderPopulated) {
+    order = doc.order as Order
+  } else if (context.order) {
+    order = context.order as Order
+  } else {
+    order = await req.payload.findByID({
+      id: orderId,
+      collection: "orders",
+      req,
+    })
+  }
+
+  if (order.total !== doc.amount) {
+    // do nothing if the amounts don't match
+    return
+  }
+
+  // mark the order as paid
+  await req.payload.update({
+    id: orderId,
+    collection: "orders",
+    data: {
+      paymentStatus: "paid",
+    },
+    req,
+  })
+}
---
+++ apps/www/src/collections/transactions/index.ts
@@ -0,0 +1,60 @@
+import { CollectionConfig } from "payload"
+import { markOrderPaid } from "./hooks/mark-order-paid"
+import { validatePaidAmount } from "./hooks/validate-amount-paid"
+
+export const transactions: CollectionConfig = {
+  slug: "transactions",
+  labels: {
+    singular: "Transaction",
+    plural: "Transactions",
+  },
+  typescript: {
+    interface: "Transaction",
+  },
+  fields: [
+    {
+      name: "order",
+      type: "relationship",
+      relationTo: "orders",
+      required: true,
+    },
+    {
+      name: "amount",
+      type: "number",
+      required: true,
+    },
+    {
+      name: "ref",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "provider",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "isTest",
+      type: "checkbox",
+      defaultValue: false,
+      admin: {
+        description: "Indicates if the transaction is a test transaction",
+        hidden: true,
+      },
+    },
+    {
+      name: "paidAt",
+      type: "date",
+      required: true,
+    },
+    {
+      name: "snapshot",
+      type: "json",
+      required: true,
+    },
+  ],
+  hooks: {
+    beforeChange: [validatePaidAmount],
+    afterChange: [markOrderPaid],
+  },
+}
