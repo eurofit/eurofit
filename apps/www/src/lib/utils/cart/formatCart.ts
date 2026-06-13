f5ad1bc6898e062eb62dbc3a26a509a0f3aa16f3
abdirazak <abdurazak.awil1@gmail.com>
2026-06-13 23:00:20 +0300

    chore(cart): remove unused formatCart util

--- apps/www/src/lib/utils/cart/formatCart.ts
+++
@@ -1,7 +0,0 @@
-import { Cart } from "@/payload-types"
-import { formatCartItem } from "./formatCartItem"
-
-export function formatCart(cart: Cart | null) {
-  if (!cart?.items) return []
-  return cart.items.map(formatCartItem)
-}
