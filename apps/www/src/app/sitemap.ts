f8b39610bdef4355f69e8412be08e866a98515c1
abdirazak <abdurazak.awil1@gmail.com>
2026-06-15 13:40:51 +0300

    feat(sitemap): add product variants to sitemap

--- apps/www/src/app/sitemap.ts
+++ apps/www/src/app/sitemap.ts
@@ -1,12 +1,14 @@
 import { getBrandsSitemap } from "@/lib/utils/brands/get-brands-sitemap"
 import { getCategoriesSitemap } from "@/lib/utils/categories/get-categories-sitemap"
+import { getProductVariantsSitemap } from "@/lib/utils/product-variants/get-product-variants-sitemap"
 import { MetadataRoute } from "next"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [brands, categories] = await Promise.all([
+  const [brands, categories, productVariants] = await Promise.all([
     getBrandsSitemap(),
     getCategoriesSitemap(),
+    getProductVariantsSitemap(),
   ])
 
-  return [...categories, ...brands]
+  return [...categories, ...brands, ...productVariants]
 }
