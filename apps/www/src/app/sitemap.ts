ecb39b4a971a133c4f5e487933a7d0871159c0b3
abdirazak <abdurazak.awil1@gmail.com>
2026-06-15 11:34:11 +0300

    refactor: disable brand/area for now

--- apps/www/src/app/sitemap.ts
+++ apps/www/src/app/sitemap.ts
@@ -1,14 +1,12 @@
-import { getBrandAreasSitemap } from "@/lib/utils/brands/get-brand-areas-sitemap"
 import { getBrandsSitemap } from "@/lib/utils/brands/get-brands-sitemap"
 import { getCategoriesSitemap } from "@/lib/utils/categories/get-categories-sitemap"
 import { MetadataRoute } from "next"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [brands, categories, brandAreas] = await Promise.all([
+  const [brands, categories] = await Promise.all([
     getBrandsSitemap(),
     getCategoriesSitemap(),
-    getBrandAreasSitemap(),
   ])
 
-  return [...categories, ...brands, ...brandAreas]
+  return [...categories, ...brands]
 }
